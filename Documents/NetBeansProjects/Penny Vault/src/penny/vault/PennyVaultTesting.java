9b8ed93c78d80fd85e86a790cb63d59a70d7b951
becar1504 <becar1504@7L8P6Y1.Student.UGDSB.ED>
2016-04-08 14:29:38 -0400

    Client code updates

--- Documents/NetBeansProjects/Penny Vault/src/penny/vault/PennyVaultTesting.java
+++ Documents/NetBeansProjects/Penny Vault/src/penny/vault/PennyVaultTesting.java
@@ -26,30 +26,46 @@ public class PennyVaultTesting {
         Coin toonie=new Coin(1.11,2);
         
         Scanner in=new Scanner(System.in);
-        
-        int choice=in.nextInt();
+        String choice;
+        int methodChoice=in.nextInt();
         System.out.println("Vault space is 1000 cubic cm");
         System.out.println("1.  add coin\n2.  take coin\n3.  Total Value\n4.  Number of a coin type\5.  Space used");
         
-        switch(choice){
+        switch(methodChoice){
             case 1: 
+                choice="add";
                 
                     System.out.println("Type the corresponding numer to chose the type of coin you would like to add");
-                    int coinChoice=in.nextInt();
-                    int addNum;
-                    switch(coinChoice){
-                        case 1:addChoice="penny";
-                            break;
-                        case 2:addChoice="nickel";
-                            break;
-                        case 3:addChoice="dime";
-                            break;
-                        case 4
+                    int addChoice=in.nextInt();
+                    String coinChoice;
+                    switch(addChoice){
+                        case 1:coinChoice="penny";
+                        break;
+                        case 2:coinChoice="nickel";
+                        break;
+                        case 3:coinChoice="dime";
+                        break;
+                        case 4:coinChoice="quarter";
+                        break;
+                        case 5:coinChoice="loonie";
+                        break;
+                        case 6:coinChoice="toonie";
+                        break;
+                        default:coinChoice="unknown currentcy";
+                        break;
                     }
-                }
+                
                 break;
-            case 2: choice=in.nextLine();
-                if
+            case 2: choice="take";
+            break;
+            case 3:choice="totalValue";
+            break;
+            case 4:choice="spaceLeft";
+            break;
+            case 5:choice="coinNum";
+            break;
+            default:choice="Please enter a valid option";
+            break;
         }
     }
     
